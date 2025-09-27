50e67397c420c3014575808538de5bf6c89e21d6
Syed Ali Asghar <aligee512@gmail.com>
2025-09-27 23:14:13 +0500

    Backend Database Set

--- backend/server-dev.js
+++ backend/server-dev.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = 'development';
 
-// Dynamically import AFTER setting NODE_ENV
 import('./server.js')
   .then(() => console.log("✅ Dev server started with NODE_ENV=development"))
   .catch(err => console.error("❌ Failed to start server:", err));
--- backend/server-prod.js
+++ backend/server-prod.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = 'production';
 
-// Dynamically import AFTER setting NODE_ENV
 import('./server.js')
   .then(() => console.log("✅ Dev server started with NODE_ENV=production"))
   .catch(err => console.error("❌ Failed to start server:", err));
