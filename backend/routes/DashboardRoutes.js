0ae5eb470a94856fe73327010dbd26bb272919bd
Syed Ali Asghar <aligee512@gmail.com>
2025-11-23 17:57:40 +0500

    fix

--- backend/routes/DashboardRoutes.js
+++ backend/routes/DashboardRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { getDashboard } from "../controllers/DashboardController.js";
-import { verifyProof } from "../controllers/ProofController.js";
 import { isAuthenticatedUser } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/", isAuthenticatedUser, getDashboard);
-router.put("/proof/verify/:id", isAuthenticatedUser, verifyProof);
 
 export default router;
